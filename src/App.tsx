eb31795ce20e45e835b98ea75c4efdee6df32da1
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-05-23 01:01:11 +0300

    added aos animation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css'
+import './styles/globals.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import {
   Card,
   CardAction,
@@ -9,10 +11,16 @@ import {
   CardTitle,
 } from "./components/ui/card"
 import { Button } from "./components/ui/button"
-import './styles/globals.css'
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+  AOS.init({
+    duration: 1000,
+    once: true,
+    offset: 120,
+  })
+
 
   return (
     <>
@@ -34,6 +42,13 @@ function App() {
           <p>Card Footer</p>
         </CardFooter>
       </Card>
+      <div>
+        <h1 data-aos="fade-up">Hello Vite + React!</h1>
+        <div data-aos="flip-left" style={{ marginTop: '100vh' }}>
+          I will animate when you scroll to me!
+        </div>
+      </div>
+
     </>
   )
 }
