3cd62dd5c4c7de59e2da3a1b260a125f5d64ca26
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-05-23 00:49:47 +0300

    added shadcn setup with red theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import './App.css'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card"
+import { Button } from "./components/ui/button"
+import './styles/globals.css'
 
 function App() {
 
   return (
     <>
       <h1>App</h1>
+      <Button variant="default">Button</Button>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+    <CardAction>Card Action</CardAction>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
     </>
   )
 }
