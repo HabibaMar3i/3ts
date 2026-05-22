70705710db2b454354cc51a5f71a24dc26d81574
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-05-23 00:55:40 +0300

    added font awesome setup

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,30 @@ import {
 } from "./components/ui/card"
 import { Button } from "./components/ui/button"
 import './styles/globals.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
 
   return (
     <>
       <h1>App</h1>
-      <Button variant="default">Button</Button>
+      <Button variant="default"><i className="fa-solid fa-house"></i> Button</Button>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
       <Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-    <CardAction>Card Action</CardAction>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          <CardAction>Card Action</CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </>
   )
 }
