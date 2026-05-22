bb55c21ce6102225970b2e562a39fd1be521777a
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-05-23 00:57:57 +0300

    added layout

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,11 @@
+import Footer from "#components/common/Footer";
+import Navbar from "#components/common/Navbar";
+
+export default function Layout() {
+    return (
+        <div>
+            <Navbar/>
+            <Footer/>
+        </div>
+    )
+}
