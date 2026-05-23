75626fe57373e19da33c5e0e318f9b5a4ba81074
HabibaMar3i <habibahassanmar3i@gmail.com>
2026-05-24 01:58:56 +0300

    add scroll to top automatically

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Footer from "#components/common/Footer"
 import Navbar from "#components/common/Navbar"
+import { useEffect } from 'react';
 
 export default function Layout() {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+    }, [location.pathname]);
+    
     return (
         <div>
             <Navbar />
